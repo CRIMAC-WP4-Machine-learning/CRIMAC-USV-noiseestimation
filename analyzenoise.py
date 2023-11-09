4740cd70093c687f6fb23de1a8c3d065a4d5deb6
Nils Olav Handegard <nils.olav.handegard@imr.no>
2023-11-09 20:30:38 +0100

    Change to python time

--- analyzenoise.py
+++ analyzenoise.py
@@ -1,16 +1,18 @@
 import pandas as pd
 import json
 import matplotlib.pyplot as plt
+import datetime
+
 noisedata = 'EchogramPlot_T20051118_06201006-20051118_11251826.json'
 
 with open(noisedata, 'r') as my_file:
     data = json.load(my_file)
 
-frames = [pd.DataFrame(index=data["time"], data={'noiseAverage': data["noiseAverage"]}),
-          pd.DataFrame(index=data["time"], data={'noiseUpperLimit': data["noiseUpperLimit"]})]
-df = pd.concat(frames, axis=1)
+time = [datetime.datetime.fromtimestamp(_t) for _t in data["time"]]
 
-#data["time"]
+frames = [pd.DataFrame(index=time, data={'noiseAverage': data["noiseAverage"]}),
+          pd.DataFrame(index=time, data={'noiseUpperLimit': data["noiseUpperLimit"]})]
+df = pd.concat(frames, axis=1)
 
 df.keys()
 df.plot()
